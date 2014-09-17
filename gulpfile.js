1cd1117f4c96f8c1d4adbcd00416fdee502bf031
Mitch R <olordhux+github@gmail.com>
2014-09-16 20:41:02 -0700

    notify on success/fail

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var sourcemaps = require('gulp-sourcemaps');
+var notify = require('gulp-notify');
 
 gulp.task('default', ['coffee']);
 
 gulp.task('coffee', function() {
   gulp.src('./src/*.coffee')
     .pipe(sourcemaps.init())
-    .pipe(coffee({}))//.on('error', gutil.log)
+    .pipe(coffee({})).on('error', console.log)
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./build'))
+    .pipe(notify({ message: 'coffeescript compiled sucessfully' }));
 });
 
 gulp.task('watch', function() {
